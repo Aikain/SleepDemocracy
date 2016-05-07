db9902747f447e905d346af5d87a4bfbf21dc0b3
Ville Nupponen <aikain@gosu.fi>
2016-05-07 12:07:57 +0300

    Add pom.xml, plugin.yml and main class

---
+++ src/main/java/fi/gosu/mc/plugins/SleepDemocracy/SleepDemocracy.java
@@ -0,0 +1,9 @@
+package fi.gosu.mc.plugins.SleepDemocracy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Aikain on 7.5.2016.
+ */
+public class SleepDemocracy extends JavaPlugin {
+}
