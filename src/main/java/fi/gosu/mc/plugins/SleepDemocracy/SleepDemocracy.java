f4a9534e5b0bd079dd7088de7746b55e8aa1830f
Ville Nupponen <aikain@gosu.fi>
2016-05-08 09:16:48 +0300

    Don't sleep if is day

--- src/main/java/fi/gosu/mc/plugins/SleepDemocracy/SleepDemocracy.java
+++ src/main/java/fi/gosu/mc/plugins/SleepDemocracy/SleepDemocracy.java
@@ -125,7 +125,7 @@ public class SleepDemocracy extends JavaPlugin implements Listener {
     private void testSleeping() {
         if (!this.SDEnable) return;
         for (World world : Bukkit.getWorlds()) {
-            if (world.getPlayers().isEmpty()) continue;
+            if (world.getPlayers().isEmpty() || world.getTime() < 12000) continue;
             int i = 0;
             for (Player player : world.getPlayers()) {
                 if (player.isSleeping()) i++;
